1ab9d0f4b9ad0f5a6f9114a3597050e167de2832
mjayy77 <billymj77@gmail.com>
2024-06-16 03:37:22 +0700

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sorting_algorithms',  
+    name='siwp2005_billy_sorting_algorithms',  
     version='1.0',  
     description='A collection of sorting algorithms',  
     long_description=open('README.md').read(),  
-    url='https://github.com/your-username/sorting_algorithms',  
+    url='https://github.com/mjayy77/Siwp-2005-billy-sort.git',  
     author='Billy',  
     author_email='billymj77@gmail.com',  
     license='MIT',  
