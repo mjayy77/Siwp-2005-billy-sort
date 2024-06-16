e5091a3f41b78e9bb45a285d55e1dffac7570aef
mjayy77 <billymj77@gmail.com>
2024-06-16 12:55:35 +0700

    modified:   dist/siwp2005_billy_sorting_algorithms-1.0-py3-none-any.whl 	modified:   dist/siwp2005_billy_sorting_algorithms-1.0.tar.gz 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 
+with open('README.rst', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
-    name='siwp2005_billy_sorting_algorithms',  
-    version='1.0',  
-    description='A collection of sorting algorithms',  
-    long_description=open('README.rst').read(),  
-    url='https://github.com/mjayy77/Siwp-2005-billy-sort.git',  
-    author='Billy',  
-    author_email='billymj77@gmail.com',  
-    license='MIT',  
-    packages=find_packages(),  
-    install_requires=[],  
+    name='siwp2005_billy_sorting_algorithms',
+    version='1.0',
+    description='A collection of sorting algorithms',
+    long_description=long_description,
+    url='https://github.com/mjayy77/Siwp-2005-billy-sort.git',
+    author='Billy',
+    author_email='billymj77@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
